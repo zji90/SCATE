0af740bab91227a2460a31c6df8253638b334913
Zhicheng Ji <zhichengji@gmail.com>
2020-08-01 15:53:41 -0400

    update

--- R/satacprocess.R
+++ R/satacprocess.R
@@ -5,6 +5,7 @@
 #' This function filters out scATAC-seq with low library size and transforms the reads into middle points of the reads.
 #' @param input Either a character vector of locations to bam files (when type is bam) or list of GRanges object of scATAC-seq reads (when type is gr).
 #' @param type Either 'bam' or 'gr'. 'bam' if the input is locations to bam files. 'gr' if the input is a list of GRanges object.
+#' @param pairsingle Either 'paired' or 'single'. Specify whether the bam file is single- or paired-end. Only works when type='bam'.
 #' @param libsizefilter Numeric variable giving the minimum library size. scATAC-seq samples with library size smaller than this cutoff will be discarded.
 #' @return GRanges object of list of GRanges object after preprocessing.
 #' @export
@@ -13,9 +14,13 @@
 #' @examples
 #' satacprocess(list(cell1=GRanges(seqnames="chr1",IRanges(start=1:100+1e6,end=1:100+1e6)),cell2=GRanges(seqnames="chr2",IRanges(start=1:100+1e6,end=1:100+1e6))),type='gr',libsizefilter=10)
 
-satacprocess <- function(input,type='bam',libsizefilter=1000) {
+satacprocess <- function(input,type='bam',pairsingle='paired',plibsizefilter=1000) {
       if (type=='bam') {
-            satac <- sapply(sapply(input,readGAlignmentPairs),GRanges)
+            if (pairsingle=='paired') {
+                  satac <- sapply(sapply(input,readGAlignmentPairs),GRanges)      
+            } else {
+                  satac <- sapply(sapply(input,readGAlignments),GRanges)      
+            }
       } else {
             satac <- input
       }
