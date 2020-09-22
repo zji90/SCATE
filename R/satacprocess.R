c9e78c244c9e1ad1ad0f6f3261096948126a1540
Zhicheng Ji <zhichengji@gmail.com>
2020-09-22 10:20:19 -0400

    Fix typo in satacprocess

--- R/satacprocess.R
+++ R/satacprocess.R
@@ -14,7 +14,7 @@
 #' @examples
 #' satacprocess(list(cell1=GRanges(seqnames="chr1",IRanges(start=1:100+1e6,end=1:100+1e6)),cell2=GRanges(seqnames="chr2",IRanges(start=1:100+1e6,end=1:100+1e6))),type='gr',libsizefilter=10)
 
-satacprocess <- function(input,type='bam',pairsingle='paired',plibsizefilter=1000) {
+satacprocess <- function(input,type='bam',pairsingle='paired',libsizefilter=1000) {
       if (type=='bam') {
             if (pairsingle=='paired') {
                   satac <- sapply(sapply(input,readGAlignmentPairs),GRanges)      
